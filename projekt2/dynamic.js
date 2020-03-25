6a61b11d27f764150c54f2e257a22c716c042614
Jackie2G <jacek.zarychta@smcebi.edu.pl>
2020-03-25 21:13:08 +0100

    wersja2

--- projekt2/dynamic.js
+++ projekt2/dynamic.js
@@ -1,26 +1,36 @@
 function add(element){
     var form = window.document.dynamicForm;
-    // We clone the add button
     var add = element.cloneNode(true);
-    // Create a new HTML tag of type "input"
     var field = document.createElement("input");
-    // The value filled in the form will be stored in an array
-    field.name = "champs[]";
-    field.type = "text";
+    field.name = "srednia_arytmetyczna";
+    field.type = "number";
     
     var rem = document.createElement("input");
     rem.value = "Usun pole";
     rem.type = "button";
-    // Add the onclick event
     rem.onclick = function onclick(event)
     {rem.parentNode.removeChild(rem);
     add.parentNode.removeChild(add);
     field.parentNode.removeChild(field)};
     
-    // We create a new element of type "p" and we insert the field inside.
     var bloc = document.createElement("p");
     bloc.appendChild(field);
     form.insertBefore(add, element);
     form.insertBefore(rem, element);
     form.insertBefore(bloc, element);
+
+    console.log(field.value);
+}
+
+function srednia(){
+    var array = document.getElementsByName("srednia_arytmetyczna");
+    var suma = 0;
+    var elementy = 0;
+    for (var i = 0; i < array.length; i++){
+        if (parseInt(array[i].value))
+            suma += parseInt(array[i].value);
+            elementy++;
+    }
+    document.getElementById("wynik").value = suma / elementy;
+    return false;
 }
