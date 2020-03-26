a3abc0be4cfa7e9a7347d087d89d3900a8c39b26
Jackie2G <jacek.zarychta@smcebi.edu.pl>
2020-03-26 13:03:06 +0100

    poczatkowe

---
+++ projekt3/sir.js
@@ -0,0 +1,7 @@
+function gowno_pierdolone(){
+const cos = document.getElementById("canvasik");
+const ctx = cos.getContext("2d")
+ctx.fillStyle="#FF0000";
+ctx.fillRect(20, 20, 150, 150);
+return false;
+}
